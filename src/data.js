68e7e1e2c5dec73c470c16c727a93cdf82422852
sakshi24022001 <55614038+sakshi24022001@users.noreply.github.com>
2022-09-24 11:49:33 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,49 @@
+import img1 from './LipMoisturizer.png'
+import img2 from './img2.png'
+import img3 from './img3.png'
+import img4 from './img4.png'
+import img5 from './img5.png'
+const list = [
+    {
+      id: 1,
+      title: "Lip Moisturizing Balm",
+      name: "Sugar Cosmetic",
+      price: 179,
+      img : img1,
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: "Phone",
+      name: "Redmi 9A",
+      price: 5000,
+      img: img2,
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: "Book",
+      name: "The Secret",
+      price: 700,
+      img: img3,
+      amount: 1,
+    },
+    {
+      id: 4,
+      title: "T-shirt",
+      name: "Hooded T-shirt",
+      price: 500,
+      img: img4,
+      amount: 1,
+    },
+    {
+      id: 5,
+      title: "Badminton",
+      name: "Yonex",
+      price: 1000,
+      img: img5,
+      amount: 1,
+    },
+  ];
+  
+  export default list;
